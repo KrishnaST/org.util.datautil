fa414d69052dc383c31ac8cce59804b1cd020626
krishna.telgave <krishna.telgave@SIL_DEV076.saas.SILASPDC.com>
2019-08-25 15:17:55 +0530

    added json serializer

--- src/module-info.java
+++ src/module-info.java
@@ -4,4 +4,8 @@
 module org.util.datautil {
 
 	exports org.util.datautil;
+	
+	requires static transitive com.fasterxml.jackson.core;
+	requires static transitive com.fasterxml.jackson.databind;
+	
 }
--- src/org/util/datautil/Strings.java
+++ src/org/util/datautil/Strings.java
@@ -10,5 +10,45 @@ public class Strings {
 		return s == null;
 	}
 
+	public static final String lessOrEqualLength(final String s, final int len) {
+		if(s == null) return "";
+		else if(s.length() > len) return s.substring(0, len);
+		else return s;
+	}
+
 	
+	public static final String makeOfLength(final String s, final int len) {
+		if(s == null) return makeOfLength("null", len);
+		else if(s.length() == len) return s;
+		else if(s.length() > len) return s.substring(0, len);
+		else {
+			final StringBuilder sb = new StringBuilder();
+			sb.append(s);
+			while (sb.length() != len) sb.append(" ");
+			return s;
+		}
+	}
+	
+	public static String padRight(String string, char padChar, int len) {
+		if (string == null || string.length() >= len) return string;
+		StringBuilder sb = new StringBuilder(len);
+		sb.append(string);
+		while (sb.length() != len) sb.append(padChar);
+		return sb.toString();
+	}
+	
+	public static String padRight(StringBuilder sb, char padChar, int len) {
+		if (sb == null || sb.length() >= len) return sb.toString();
+		while (sb.length() != len) sb.append(padChar);
+		return sb.toString();
+	}
+
+	public static String padLeft(String string, char padChar, int len) {
+		if (string == null || string.length() >= len) return string;
+		StringBuilder sb = new StringBuilder(len);
+		while (sb.length() != (len - string.length())) sb.append(padChar);
+		sb.append(string);
+		return sb.toString();
+	}
+
 }
